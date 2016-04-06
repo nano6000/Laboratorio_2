c21496df97cb9a387ddd27743f8baf55160cbf74
Esteban <nano.95.eb@gmail.com>
2016-04-06 00:15:05 -0600

    Calculo aproximado de pi con presicion de 8 decimales

--- Lab2/lab2.asm
+++ Lab2/lab2.asm
@@ -4,10 +4,10 @@
 ;Objetivo: Calcular PI
 ;Entradas: -------
 ;Salidas: el valor aproximado de PI, con presicion de decimales
-
+bits 32
 section .bss
 
-denominador resw 1
+denominador resq 1
 numerador resw 1
 PI resq 1
 
@@ -18,44 +18,62 @@ section .text
 _start:
 	mov word [denominador],1
 	mov word [numerador],4
-	fild word [denominador] ;carga el nuevo denominador
-	fidivr word [numerador]	;divide el numerador entre el 
-								;denominador y lo guarda en el tope actual 
-								;de la pila
-	xor eax,eax				;Limpia el registro eax para alternar entre
-								;suma y resta
-								;0x00 -> resta || 0xFF -> suma
-	xor ecx,ecx				;Limpia el ecx para llevar un contador de
-								;la cantidad de series que se realizan
+	fild qword [denominador]	;carga el nuevo denominador
+	fidivr word [numerador]		;divide el numerador entre el 
+									;denominador y lo guarda en el tope actual 
+									;de la pila
+	xor eax,eax					;Limpia el registro eax para alternar entre
+									;suma y resta
+									;0x00 -> resta || 0xFF -> suma
+	xor ecx,ecx					;Limpia el ecx para llevar un contador de
+									;la cantidad de series que se realizan
+	xor edx,edx					;Limpia el edx para llevar un contador auxiliar
+									;de la cantidad de series que se realizan
+									;para llevar valores
 	
 incremento:
-	mov ebx,denominador		;copia direccion de denominador en ebx
-	add word [ebx],2		;suma 2 al valor de denominador
+	mov ebx,denominador			;copia direccion de denominador en ebx
+	add dword [ebx],2			;suma 2 al valor de denominador
 actualizar:	
-	fild word [denominador] ;carga el nuevo denominador
-	fidivr word [numerador]	;divide el numerador entre el 
-								;denominador y lo guarda en el tope actual 
-								;de la pila
+	fild qword [denominador] 	;carga el nuevo denominador
+	fidivr word [numerador]		;divide el numerador entre el 
+									;denominador y lo guarda en el tope actual 
+									;de la pila
 							
-	cmp eax,0x00			;decide si lo que sigue es una suma
-	jne suma					;o una resta
+	cmp eax,0x00				;decide si lo que sigue es una suma
+	jne suma						;o una resta
 resta:
-	fsubp ST1,ST0			;resta el valor del tope de la pila y lo 
-								;almacena en el valor anterior al tope
-								;y hace un pop a la pila
-	xor eax,0xFF			;Cambia el valor del eax a 0xFF
-	jmp continuar
+	fsubp ST1,ST0				;resta el valor del tope de la pila y lo 
+									;almacena en el valor anterior al tope
+									;y hace un pop a la pila
+	xor eax,0xFF				;Cambia el valor del eax a 0xFF
+	jmp comprobar
 suma:
-	faddp ST1,ST0			;suma el valor del tope de la pila y lo 
-								;almacena en el valor anterior al tope
-								;y hace un pop a la pila
-	xor eax,0xFF			;Cambia el valor del eax a 0x00
-	inc ecx					;Incrementa el contador
+	faddp ST1,ST0				;suma el valor del tope de la pila y lo 
+									;almacena en el valor anterior al tope
+									;y hace un pop a la pila
+	xor eax,0xFF				;Cambia el valor del eax a 0x00
+	inc ecx						;Incrementa el contador
 	
+comprobar:
+	;cmp edx,16375
+	;je aumentar_tope
 continuar:
-	cmp ecx,1000
-	jne incremento
+	cmp ecx,500000000
+	;cmp ecx,16384
+	je calcular
+	jmp incremento
+
+aumentar_tope:
+	fincstp
+	jmp continuar
 	
+calcular:
+	;faddp ST1,ST0
+	;faddp ST1,ST0
+	;faddp ST1,ST0
+	;fidiv word [numerador]
+	fild qword [denominador]
 imprimir:
 	fst qword [PI]
 
